19aa2e8b7f636b33081399be981e504286d8fe0d
NadeemAkhtar9 <akhtar.nadeem99099@gmail.com>
2021-01-24 20:58:15 +0530

    added models,urls,views,admin

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date =models.DateTimeField()
+    link = models.URLField()
+
+def __str__(self):
+    return self.title
+    
